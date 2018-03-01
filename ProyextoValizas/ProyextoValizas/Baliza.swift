14e586ab68c8b1a2476cdedaf163178b1da2cf3c
Unknown <aritz.martinezh@gmail.com>
2018-03-01 21:31:25 +0100

    liada

---
+++ ProyextoValizas/ProyextoValizas/Baliza.swift
@@ -0,0 +1,19 @@
+//
+//  Baliza.swift
+//  ProyextoValizas
+//
+//  Created by  on 1/3/18.
+//  Copyright © 2018 Carlos Hernández. All rights reserved.
+//
+
+
+import Foundation
+
+import CoreLocation
+
+struct Baliza {
+    var uuid: String
+    var major: CLBeaconMajorValue
+    var minor: CLBeaconMinorValue
+    var id: String
+}
--- ProyextoValizas/ProyextoValizas/ViewController.swift
+++ ProyextoValizas/ProyextoValizas/ViewController.swift
@@ -14,52 +14,109 @@ import CoreLocation
 class ViewController: UIViewController, SRCountdownTimerDelegate , CLLocationManagerDelegate{
     
 let locationManager = CLLocationManager()
-    
+    var region: CLBeaconRegion?
+    var manager: CLLocationManager?
     var nClase = " "
-    
+    var funciona = " "
+    let miBaliza = Baliza(uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e",
+                          major: 58387,
+                          minor: 33802,
+                          id: "com.jaureguialzo.ejemplobeacon")
     @IBOutlet weak var contadorSOS: SRCountdownTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         contadorSOS.delegate = self
+        enableLocationServices()
+        
         
        
+        manager?.delegate = self
+        self.manager = CLLocationManager()
+        region = CLBeaconRegion(proximityUUID: UUID(uuidString: miBaliza.uuid)!, identifier: miBaliza.id)
+        
+        
+         locationManager.delegate = self
         
     }
     
+   func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        
+        manager.startRangingBeacons(in: region as! CLBeaconRegion) // Para pruebas
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        manager.startRangingBeacons(in: region as! CLBeaconRegion)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        manager.stopRangingBeacons(in: region as! CLBeaconRegion)
+     
+    }
+    
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if(beacons.count == 0) { return }
+        if(beacons.count == 0) {return}
         
         let beacon = beacons.last!
         
         if (beacon.proximity == .unknown) {
-            nClase = "Desconocida"
+            funciona = "Desconocida"
             return
         } else if (beacon.proximity == .immediate) {
-            nClase = "Muy cerca \(beacon.major)"
-            
+            funciona = "Muy cerca"
             
         } else if (beacon.proximity == .near) {
-            nClase = "Cerca"
+            funciona = "Cerca"
         } else if (beacon.proximity == .far) {
-            nClase = "Lejos"
+            funciona = "Lejos"
         }
         
        
         
     }
+    func enableLocationServices() {
+        //locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            nClase = "No tenemos acceso a la localización"
+            
+            break
+            
+        case .authorizedWhenInUse:
+            nClase = "Localización sólo al usar la aplicación"
+            
+            break
+            
+        case .authorizedAlways:
+            nClase = "Localización permanente"
+            
+            break
+        }
+    
+    }
 
+   
+    
     
     @IBAction func test(_ sender: Any) {
         
-        locationManager.delegate = self
-        
-        locationManager.requestWhenInUseAuthorization()
-        
-        
+        self.manager?.requestAlwaysAuthorization()
+        self.manager?.startMonitoring(for: self.region!)
+        locationManager.startUpdatingLocation()
+    
         let alertController = UIAlertController(title: "iOScreator", message:
-            nClase, preferredStyle: UIAlertControllerStyle.alert)
+            nClase+funciona, preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
         
         self.present(alertController, animated: true, completion: nil)
@@ -96,4 +153,3 @@ let locationManager = CLLocationManager()
 
 
 }
-
