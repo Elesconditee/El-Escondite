274c486c223378cef81bc00f66649a2ed8233618
Unknown <aritz.martinezh@gmail.com>
2018-03-01 22:04:17 +0100

    Ultima Hora

--- ProyextoValizas/ProyextoValizas/ViewController.swift
+++ ProyextoValizas/ProyextoValizas/ViewController.swift
@@ -14,72 +14,36 @@ import CoreLocation
 class ViewController: UIViewController, SRCountdownTimerDelegate , CLLocationManagerDelegate{
     
 let locationManager = CLLocationManager()
-    var region: CLBeaconRegion?
+    var beaconRegion: CLBeaconRegion!
     var manager: CLLocationManager?
-    var nClase = " "
-    var funciona = " "
+ 
+   
+    
     let miBaliza = Baliza(uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e",
                           major: 58387,
                           minor: 33802,
                           id: "com.jaureguialzo.ejemplobeacon")
+    
+   
+    
     @IBOutlet weak var contadorSOS: SRCountdownTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         contadorSOS.delegate = self
-        enableLocationServices()
-        
-        
-       
-        manager?.delegate = self
+
         self.manager = CLLocationManager()
-        region = CLBeaconRegion(proximityUUID: UUID(uuidString: miBaliza.uuid)!, identifier: miBaliza.id)
-        
-        
-         locationManager.delegate = self
+        manager?.delegate = self
+        enableLocationServices()
+    
+         beaconRegion = CLBeaconRegion(proximityUUID: UUID(uuidString: miBaliza.uuid)!, identifier: miBaliza.id)
         
     }
     
-   func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
-        
-        manager.startRangingBeacons(in: region as! CLBeaconRegion) // Para pruebas
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        manager.startRangingBeacons(in: region as! CLBeaconRegion)
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        manager.stopRangingBeacons(in: region as! CLBeaconRegion)
-     
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if(beacons.count == 0) {return}
-        
-        let beacon = beacons.last!
-        
-        if (beacon.proximity == .unknown) {
-            funciona = "Desconocida"
-            return
-        } else if (beacon.proximity == .immediate) {
-            funciona = "Muy cerca"
-            
-        } else if (beacon.proximity == .near) {
-            funciona = "Cerca"
-        } else if (beacon.proximity == .far) {
-            funciona = "Lejos"
-        }
-        
        
         
-    }
+    
     func enableLocationServices() {
         //locationManager.delegate = self
         
@@ -89,17 +53,17 @@ let locationManager = CLLocationManager()
             break
             
         case .restricted, .denied:
-            nClase = "No tenemos acceso a la localización"
+            print("No tenemos acceso a la localización")
             
             break
             
         case .authorizedWhenInUse:
-            nClase = "Localización sólo al usar la aplicación"
+            print("Localización sólo al usar la aplicación")
             
             break
             
         case .authorizedAlways:
-            nClase = "Localización permanente"
+           print("Localización permanente")
             
             break
         }
@@ -111,12 +75,10 @@ let locationManager = CLLocationManager()
     
     @IBAction func test(_ sender: Any) {
         
-        self.manager?.requestAlwaysAuthorization()
-        self.manager?.startMonitoring(for: self.region!)
-        locationManager.startUpdatingLocation()
+        self.manager?.startMonitoring(for: self.beaconRegion!)
     
         let alertController = UIAlertController(title: "iOScreator", message:
-            nClase+funciona, preferredStyle: UIAlertControllerStyle.alert)
+            "Aqui el mensajito", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
         
         self.present(alertController, animated: true, completion: nil)
@@ -151,5 +113,53 @@ let locationManager = CLLocationManager()
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager,
+                         didStartMonitoringFor region: CLRegion) {
+        locationManager.requestState(for: region)
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didDetermineState state: CLRegionState,
+                         for region: CLRegion) {
+        if state == .inside {
+            locationManager.startRangingBeacons(in: beaconRegion)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didRangeBeacons beacons: [CLBeacon],
+                         in region: CLBeaconRegion) {
+        
+        guard let nearestBeacon = beacons.first else { return }
+        
+        switch nearestBeacon.proximity {
+        case .far:
+            print("Lejos")
+        case .immediate:
+            view.backgroundColor = .green
+            print("Cerca")
+        case .near:
+            view.backgroundColor = .yellow
+            print("Medio")
+        default:
+            view.backgroundColor = .red
+            print("Otro")
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, rangingBeaconsDidFailFor region: CLBeaconRegion, withError error: Error) {
+        print(error.localizedDescription)
+    }
 
 }
